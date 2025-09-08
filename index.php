d6870826ce9c2dfcda9b71a8f317d49c7aef52e9
Hebert Bratefixe Alquimim <hebert@uni9.pro.br>
2025-09-08 20:39:02 +0000

    08-09-25

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <h1>Testando php - 27/08/25</h1>
     <?php
     echo "Comando pelo php novo<br>";
-    echo "Uma nova linha";
+    echo "Uma nova linha<br>";
     ?>
     <br>
     <a href="pg1.php">PG1</a>
