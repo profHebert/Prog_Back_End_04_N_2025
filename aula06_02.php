d3d6b93ceb35f15336001bb3ddde6408e93e5c9e
Hebert Bratefixe Alquimim <hebert@uni9.pro.br>
2025-09-18 00:44:24 +0000

    Aula6

--- aula06_02.php
+++ aula06_02.php
@@ -13,5 +13,32 @@
     Apresentar as operações aritméticas
     e de comparação entre esses dois números
     -->
+    <form method="post" action="#">
+    Número a<input type="number" name="a" required><br>
+    Número b<input type="number" name="b" required><br>
+    <input type="submit" value="Enviar!">
+    </form>
+    <hr>
+    <?php
+    if(count($_POST)==0)exit;
+    $a=$_POST["a"];
+    $b=$_POST["b"];
+    echo "Adição:".($a + $b);
+    echo "<br>Subtração:".($a - $b);
+    echo "<br>Multiplicação:".($a * $b);
+    echo "<br>Divisão:".($a / $b);
+    echo "<br>Módulo:".($a % $b);
+    echo "<br>Exponenciação:".($a ** $b);
+    echo "<hr>";
+    echo "Igualdade = ".(($a==$b)?"V":"F")."<br>";//
+    echo "Idêntico = ".($a===$b)."<br>";//
+    echo "Não igual = ".($a!=$b)."<br>";//
+    echo "Não Idêntico = ".($a!==$b)."<br>";//
+    echo "Menor: ".($a<$b)."<br>";//
+    echo "Menor ou igual: ".($a<=$b)."<br>";//
+    echo "Maior: ".($a>$b)."<br>";//
+    echo "Maior ou igual: ".($a>=$b)."<br>";//
+    ?>
+  
 </body>
 </html>
