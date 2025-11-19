7bfa146956989b134765cc6f07cf56027a5a8304
Hebert Bratefixe Alquimim <hebert@uni9.pro.br>
2025-11-19 22:52:16 +0000

    aula10

---
+++ aula10_01.php
@@ -0,0 +1,9 @@
+<?php
+if (session_status() == PHP_SESSION_NONE) { session_start(); } 
+
+// Salvando informações do usuário na sessão
+$_SESSION['usuario'] = 'joao';
+$_SESSION['email'] = 'joao@email.com';
+
+echo 'Dados da sessão armazenados!';
+?>
